2de02323f55793545f11443e1e480d29200a5111
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-01-30 12:03:20 +0100

    1. The Hero Editor: Create the heroes component
    ng generate component heroes

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import {Component, OnInit} from '@angular/core';
+
+// @Component is a decorator function that specifies the Angular metadata for the component
+@Component({
+    selector: 'app-heroes',                   // the component's CSS element selector
+    templateUrl: './heroes.component.html',   // the location of the component's template file
+    styleUrls: ['./heroes.component.sass']    // the location of the component's private CSS styles
+})
+
+export class HeroesComponent implements OnInit { // Always export the component class so you can import it elsewhere
+
+    constructor() {
+    }
+
+    // Angular calls ngOnInit shortly after creating a component
+    // It's a good place to put initialization logic
+    ngOnInit() {
+    }
+
+}
