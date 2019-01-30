62a0e6bc0c5dd060889861e43063447f5c29e6c0
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-01-30 15:08:05 +0100

    1. The Hero Editor: Edit the hero: AppModule & FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import {NgModule} from '@angular/core'
 
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
+import {FormsModule} from '@angular/forms' // <-- NgModel lives here
 import {HeroesComponent} from './heroes/heroes.component'
 
 @NgModule({
+    // AppModule declares both application components, AppComponent and HeroesComponent
     declarations: [
         AppComponent,
-        HeroesComponent
+        HeroesComponent // Every component must be declared in exactly one NgModule
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         AppRoutingModule
     ],
     providers: [],
