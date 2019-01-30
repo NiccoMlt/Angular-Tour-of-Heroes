a52fb1e83eaec9e46ded5fab2b840394d6132d40
Niccolò Maltoni <nicco.mlt@gmail.com>
2019-01-30 17:10:09 +0100

    4. Services: Create MessagesComponent & Create the MessageService
    ng generate component messages
    ng generate service message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms' // <-- NgModel lives here
 import {HeroesComponent} from './heroes/heroes.component'
-import {HeroDetailComponent} from './hero-detail/hero-detail.component'
+import {HeroDetailComponent} from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component'
 
 @NgModule({
     // AppModule declares both application components, AppComponent and HeroesComponent
     declarations: [
         AppComponent,
         HeroesComponent,
-        HeroDetailComponent // Every component must be declared in exactly one NgModule
+        HeroDetailComponent,
+        MessagesComponent // Every component must be declared in exactly one NgModule
     ],
     imports: [
         BrowserModule,
