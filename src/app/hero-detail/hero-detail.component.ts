da73636ba78ba3282a4ea74275a7aa54e6bd5980
NiccoMlt <nicco.mlt@gmail.com>
2019-01-30 22:33:56 +0100

    5. Routing: Routable HeroDetailComponent

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core'
-import { Hero } from '../hero'
+import {Component, OnInit, Input} from '@angular/core'
+import {ActivatedRoute} from '@angular/router'
+import {Location} from '@angular/common'
+
+import {HeroService} from '../hero.service'
+import {Hero} from '../hero'
 
 @Component({
     selector: 'app-hero-detail',
@@ -11,10 +15,21 @@ import { Hero } from '../hero'
 export class HeroDetailComponent implements OnInit {
     @Input() hero: Hero
 
-    constructor() {
+    constructor(
+        private route: ActivatedRoute, // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent
+        private heroService: HeroService, // The HeroService gets hero data from the remote server
+        private location: Location // The location is an Angular service for interacting with the browser
+    ) {
     }
 
     ngOnInit() {
+        this.getHero()
     }
 
+    getHero(): void {
+        const id = +this.route.snapshot.paramMap.get('id')
+        this.heroService
+            .getHero(id)
+            .subscribe(hero => this.hero = hero)
+    }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,12 @@ export class HeroService {
     constructor(private messageService: MessageService) {
     }
 
+    getHero(id: number): Observable<Hero> {
+        // TODO: send the message _after_ fetching the hero
+        this.messageService.add(`HeroService: fetched hero id=${id}`)
+        return of(HEROES.find(hero => hero.id === id))
+    }
+
     getHeroes(): Observable<Hero[]> {
         const heroes: Observable<Hero[]> = of(HEROES)
         heroes.subscribe(() => this.messageService.add('HeroService: fetched heroes'))
